0842fe699cb850f9456c38ad0862a3fc02f90d59
Padzx <padzxgabriel@gmail.com>
2024-05-02 19:55:43 -0300

    Changes - Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './index.scss';
 import Hero from './components/hero/hero';
 import Branding from './components/branding/branding';
+import Navbar from './components/navbar/navbar';
 
 export default function App() {
   return (
     <div>
-      <section>    
+      <section>
+        <Navbar />
         <Hero />
       </section>
       <section>
