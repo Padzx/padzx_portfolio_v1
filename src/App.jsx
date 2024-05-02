d04e676d20e71041edbec2f5f949db186611cfe3
Padzx <padzxgabriel@gmail.com>
2024-05-02 19:29:26 -0300

    Portfolio-React-Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.scss';
-import Redirect from './untils/pageLink'; 
+import Redirect from './utils/pageLink'; 
 import Home from './pages/home';
 
 
