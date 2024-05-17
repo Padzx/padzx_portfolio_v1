6912f200e443cf1a347489c266b08d8ca14477a5
Padzx <padzxgabriel@gmail.com>
2024-05-17 20:59:40 -0300

    update portfolio

--- src/components/skills/skills-stack/skillsFrontend.jsx
+++ src/components/skills/skills-stack/skillsFrontend.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { FaReact, FaHtml5, FaCss3, FaAws, FaGitAlt } from "react-icons/fa";
+import { FaReact, FaHtml5, FaCss3, FaAws, FaGitAlt, FaPython } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import { GrDocker } from "react-icons/gr";
-import { SiKubernetes } from "react-icons/si";
+import { SiKubernetes, SiGithubactions } from "react-icons/si";
 
 
 const SkillsFrontend = () => {
@@ -12,10 +12,12 @@ const SkillsFrontend = () => {
             <div className="grid"><FaHtml5 className="tech-icons" /></div>
             <div className="grid"><FaCss3 className="tech-icons" /></div>
             <div className="grid"><IoLogoJavascript className="tech-icons" /></div>
+            <div className="grid"><SiGithubactions className="tech-icons" /></div>
             <div className="grid docker"><GrDocker className="tech-icons" /></div>
             <div className="grid k8s"><SiKubernetes className="tech-icons" /></div>
             <div className="grid aws"><FaAws className="tech-icons" /></div>
             <div className="grid git"><FaGitAlt className="tech-icons" /></div>
+            <div className="grid python"><FaPython className="tech-icons" /></div>
         </div>
     );
 }
